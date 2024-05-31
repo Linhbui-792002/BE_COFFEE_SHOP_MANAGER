fc632a24c3e2ace6754a956f2f1618f58ddf179f
LinhBX <Linhbxhe160309@fpt.edu.vn>
2024-05-31 17:08:25 +0700

    fix/dotenv-cors

--- src/helpers/corsOptions.js
+++ src/helpers/corsOptions.js
@@ -1,9 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config()
 const whiteList = [process.env.CORS_WHITELIST_1, process.env.CORS_WHITELIST_2];
 
 const corsOptions = (req, callback) => {
   let options;
-  console.log(req.header('Origin'));
-  if (whiteList.indexOf(req.header('Origin')) !== -1) {
+  console.log(req.header('Origin'), whiteList);
+
+  if (whiteList.includes(req.header('Origin'))) {
     console.log('origin true');
     options = { origin: true };
   } else {
